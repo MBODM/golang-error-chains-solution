a796aac8981effbd73d7fa10a56bce8b6c7d873f
MBODM <webmaster@mbodm.com>
2022-06-06 06:59:22 +0200

    Finished example

--- src/main.go
+++ src/main.go
@@ -28,27 +28,29 @@ func main() {
 	level2Err := fmt.Errorf("[Error in L2]: Wrap L1Err %w", level1Err)
 	level3Err := &CustomError{"[Error in L3]: Wrap L2Err", level2Err}
 	level4Err := fmt.Errorf("[Error in L4]: Wrap L3Err %w", level3Err)
-
 	printAllWrappedErrors(level4Err)
-	fmt.Println("----------")
-	printCustomError(level4Err)
-	fmt.Println("----------")
+	printCustomErrorOnly(level4Err)
+	printCustomErrorIncludingAllWrappedErrors(level4Err)
+	// Console output:
+	// Print1 --> [Error in L4]: Wrap L3Err [Error in L3]: Wrap L2Err [Error in L2]: Wrap L1Err [Error in L1]: Boom
+	// Print2 --> [Error in L3]: Wrap L2Err
+	// Print3 --> [Error in L3]: Wrap L2Err [Error in L2]: Wrap L1Err [Error in L1]: Boom
 }
 
 func printAllWrappedErrors(topLevelError error) {
-	fmt.Println(topLevelError)
+	fmt.Println("Print1 -->", topLevelError)
 }
 
-func printCustomError(topLevelError error) {
+func printCustomErrorOnly(topLevelError error) {
 	var e *CustomError
 	if errors.As(topLevelError, &e) {
-		fmt.Println(e.Msg) // <-- This is the difference
+		fmt.Println("Print2 -->", e.Msg) // <-- This is the difference
 	}
 }
 
-func printCustomErrorIncludingAllBelow(topLevelError error) {
+func printCustomErrorIncludingAllWrappedErrors(topLevelError error) {
 	var e *CustomError
 	if errors.As(topLevelError, &e) {
-		fmt.Println(e) // <-- This is the difference
+		fmt.Println("Print3 -->", e) // <-- This is the difference
 	}
 }
